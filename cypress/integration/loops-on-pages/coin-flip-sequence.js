d9f94e29b240d94dc1f869b11d7c168b042569bd
Gabriel Dias <gabriel.silva@encora.com>
2022-07-06 09:19:06 -0300

    Fixed the requested issues

--- cypress/integration/loops-on-pages/coin-flip-sequence.js
+++ cypress/integration/loops-on-pages/coin-flip-sequence.js
@@ -3,11 +3,15 @@ import {LoopsUtils} from '../../support/loops-Utils/loops-utils'
 const loopsUtils = new LoopsUtils()
 
 describe('Coin Flip Sequence', () => {
+    const loopLimit = 10
+    const coinsNum = 5
+    const coinCurrency = 'Brazilian 1 Real'
+
     beforeEach('Accessing the Coin Flip page', () => {
         cy.visit('https://www.random.org/coins/')
     })
 
     it('Seeking for a coin flip sequence', () => {
-        loopsUtils.coinFlip(5, 2, 'Brazilian 1 Real')
+        loopsUtils.coinFlip(loopLimit, coinsNum, coinCurrency)
     })
 })
--- cypress/support/loops-Utils/loops-utils.js
+++ cypress/support/loops-Utils/loops-utils.js
@@ -3,20 +3,31 @@ export class LoopsUtils {
     var loop_limit = loop_limit;
 
     cy.get(intergerLocator).then(($value) => {
+      var sequenceCount = 0;
       const raw_numbers = $value.text();
       const splitted_numbers = raw_numbers.split("\n");
       if (loop_limit !== 0) {
         if (
-          parseInt(splitted_numbers[0]) + 1 ===
-          parseInt(splitted_numbers[1])
+          this.sequenceBetweenTwoNumbers(
+            splitted_numbers[0],
+            splitted_numbers[1]
+          )
         ) {
           loop_limit--;
-          expect(true).to.eq(true);
+          sequenceCount++
         } else {
           cy.contains("Again!").click();
           loop_limit--;
           this.sequenceFinder(intergerLocator, loop_limit);
         }
+      } else {
+        cy.get("body").then(()=>{
+          if(sequenceCount > 0){
+            cy.log("At least one sequence was shown. Test passed successfully!")
+          } else {
+            throw new Error("No sequence was shown")
+          }
+        })
       }
     });
   }
@@ -33,42 +44,54 @@ export class LoopsUtils {
     this.sequenceFinder(".data", loop_limit);
   }
 
+  sequenceBetweenTwoNumbers(a, b) {
+    if (parseInt(a) + 1 === parseInt(b)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   coinCheck(loop_limit) {
     var loop_limit = loop_limit;
     cy.get(":nth-child(9) > :nth-child(1)")
       .get("img")
       .then(($first) => {
-        var coins_count = 1;
+        var coins_check = true;
+        var coins_count = 0;
         cy.get(":nth-child(9)")
           .get("img")
           .each(($value) => {
             if ($value.attr("title") !== $first.attr("title")) {
-              cy.log($value.attr("title"));
-              coins_count = 0;
+              coins_check = false;
             }
           });
 
         cy.get("body").then(() => {
-          if (coins_count !== 0) {
+          if (coins_check) {
             cy.log(`All coin faces are ${$first.attr("title")}`);
-            expect(true).to.eq(true);
-          } else {
-            cy.log(`There is at least one coin different`);
+            coins_count++;
           }
         });
 
         loop_limit--;
-        if (loop_limit > 0) {
-          cy.contains("Flip Again").click();
-          this.coinCheck(loop_limit);
-        }
+        cy.get("body").then(() => {
+          if (loop_limit > 0) {
+            cy.contains("Flip Again").click();
+            this.coinCheck(loop_limit);
+          } else {
+            if (coins_count === 0) {
+              throw new Error(`There is at least one coin different`)
+            }
+          }
+        })
       });
   }
 
   coinFlip(loop_limit, coins_num, currency) {
     cy.get('[name="num"]').select(`${coins_num}`);
     cy.get('[name="cur"]').select(`${currency}`);
-    cy.contains("Flip Coin(s)").countRolls, pickedNumber, countShowclick();
+    cy.contains("Flip Coin(s)").click();
 
     this.coinCheck(loop_limit);
   }
@@ -95,8 +118,8 @@ export class LoopsUtils {
               `The number ${pickedNumber} was shown how much was requested`
             );
           } else {
-            cy.log("The number was not shown enough, please try again!");
-            cy.log(`${auxCountShow} remaining times.`);
+            throw new Error("The number was not shown enough, please try again!")
+            
           }
         });
       }
