b79d406ef2fa6fbeaa336ffe71867221681d6fa2
Gabriel Dias <gabriel.silva@encora.com>
2022-09-06 10:05:11 -0300

    fruis GraphQL mutation imp

--- cypress/integration/graphql-testing/test-description.js
+++ cypress/integration/graphql-testing/test-description.js
@@ -41,7 +41,7 @@ describe("GraphQL trials", () => {
       }
     }`;
 
-    var fruitsQuery = `{
+  var fruitsQuery = `{
         fruit(id: 13){
           fruit_name
           tree_name
@@ -51,9 +51,25 @@ describe("GraphQL trials", () => {
           origin
           description
         }
-      }`
+      }`;
 
-  it("Weather GraphQL Request", () => {
+  var fruitsMutation = `mutation {
+        addFruit (id: 98, scientific_name:"btt", tree_name:"tree", fruit_name: "fruta", family:"familia", origin:"origin", description:"descrição", bloom:"bloom", maturation_fruit:"maturation", life_cycle:"cicle", climatic_zone:"climatic zone") {
+          id
+          scientific_name
+          tree_name
+          fruit_name
+          family
+          origin
+          description
+          bloom
+          maturation_fruit
+          life_cycle
+          climatic_zone
+        }
+      }`;
+
+  it("Weather GraphQL Request (Query)", () => {
     cy.request({
       method: "POST",
       url: "https://graphql-weather-api.herokuapp.com/",
@@ -65,7 +81,7 @@ describe("GraphQL trials", () => {
     });
   });
 
-  it("Rick and Morty GraphQL Request", () => {
+  it("Rick and Morty GraphQL Request (Query)", () => {
     cy.request({
       method: "POST",
       url: "https://rickandmortyapi.com/graphql",
@@ -77,15 +93,27 @@ describe("GraphQL trials", () => {
     });
   });
 
-  it('Fruits GraphQL Request', () => {
+  it("Fruits GraphQL Request (Query)", () => {
     cy.request({
-        method: "POST",
-        url: "https://fruits-api.netlify.app/graphql",
-        body: {
-            query: fruitsQuery
-        }
-    }).then(response => {
-        cy.log(response.body)
-    })
-  })
+      method: "POST",
+      url: "https://fruits-api.netlify.app/graphql",
+      body: {
+        query: fruitsQuery,
+      },
+    }).then((response) => {
+      cy.log(response.body);
+    });
+  });
+
+  it("Fruits GraphQL Request (mutation)", () => {
+    cy.request({
+      method: "POST",
+      url: "https://fruits-api.netlify.app/graphql",
+      body: {
+        query: fruitsMutation,
+      },
+    }).then((response) => {
+      cy.log(response.body);
+    });
+  });
 });
